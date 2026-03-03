96fb57b008881ba592a4ae5ada502b422cc02ea4
DI MI NOMBRE <juanescobar321371@correo.itm.edu.co>
2026-03-02 23:10:11 -0500

    Bug: No quieren imprimir los metodos

--- src/index.php
+++ src/index.php
@@ -1,6 +1,22 @@
 <?php
-echo 'Hablame';
-
 $name= "Carlos";
+$op=$a*$b;
+
+function saludo($greeting, $name){
+	$greeting= "Ey ". $name. " Porque tan serio";
+    echo $greeting;
+}
+
+echo saludo($greeting, $name);
+
+function product($a,$b,$op){
+    $a=25;
+    $b=5;
+
+    echo $op;
+   
+}
+
+echo "Ya veo porque estas pesando " .product($a,$b,$op);
 
 ?>
